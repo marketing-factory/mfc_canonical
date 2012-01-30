fdcacc85e67e7052ce5f75489e2f009aaeb98a11
sfsmfc <sfsmfc@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-30 11:53:24 +0000

    initial release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mfc_canonical/trunk/mfc_canonical@56990 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "mfc_canonical".
+#
+# Auto generated 16-12-2011 09:47
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Canonical',
+	'description' => 'Build a canonical tag depending on configuration',
+	'category' => 'Marketing Factory',
+	'author' => 'Sebastian Fischer, Simon Schmidt',
+	'author_email' => 'sf@marketing-factory.de, sfs@marketing-factory.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => 'pages',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author_company' => 'Marketing Factory Consulting GmbH',
+	'version' => '1.0.1',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:8:{s:16:"ext_autoload.php";s:4:"cc1f";s:12:"ext_icon.gif";s:4:"3527";s:14:"ext_tables.php";s:4:"392f";s:14:"ext_tables.sql";s:4:"6de1";s:26:"Classes/User/Canonical.php";s:4:"adf8";s:38:"Configuration/TypoScript/constants.txt";s:4:"2b06";s:34:"Configuration/TypoScript/setup.txt";s:4:"72b1";s:44:"Resources/Private/Language/locallang_tca.xml";s:4:"c23a";}',
+	'suggests' => array(
+	),
+);
+
+?>
