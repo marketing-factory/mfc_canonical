10c864392b173e508958db0c6ece8557c77d3905
sfsmfc <sfsmfc@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-02-16 17:05:03 +0000

    update version to 1.1.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mfc_canonical/trunk@58037 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "mfc_canonical".
 #
-# Auto generated 16-12-2011 09:47
+# Auto generated 16-02-2012 18:00
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => 'Marketing Factory Consulting GmbH',
-	'version' => '1.1.1',
+	'version' => '1.1.2',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:16:"ext_autoload.php";s:4:"cc1f";s:12:"ext_icon.gif";s:4:"3527";s:14:"ext_tables.php";s:4:"392f";s:14:"ext_tables.sql";s:4:"6de1";s:26:"Classes/User/Canonical.php";s:4:"adf8";s:38:"Configuration/TypoScript/constants.txt";s:4:"2b06";s:34:"Configuration/TypoScript/setup.txt";s:4:"72b1";s:44:"Resources/Private/Language/locallang_tca.xml";s:4:"c23a";}',
+	'_md5_values_when_last_written' => 'a:8:{s:16:"ext_autoload.php";s:4:"cc1f";s:12:"ext_icon.gif";s:4:"3527";s:14:"ext_tables.php";s:4:"392f";s:14:"ext_tables.sql";s:4:"6de1";s:26:"Classes/User/Canonical.php";s:4:"1f48";s:38:"Configuration/TypoScript/constants.txt";s:4:"2b06";s:34:"Configuration/TypoScript/setup.txt";s:4:"72b1";s:44:"Resources/Private/Language/locallang_tca.xml";s:4:"c23a";}',
 	'suggests' => array(
 	),
 );
