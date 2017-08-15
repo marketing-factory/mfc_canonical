3c6c6c122e8b779b9ee138dc733a3845240f37db
Sebastian Fischer <typo3@evoweb.de>
2017-08-15 14:24:17 +0200

    Make compatible with TYPO3 8

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') or die();
+defined('TYPO3_MODE') || die();
 
 $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',tx_mfccanonical_canonical';
 $GLOBALS['TYPO3_CONF_VARS']['FE']['pageOverlayFields'] .= ',tx_mfccanonical_canonical';
